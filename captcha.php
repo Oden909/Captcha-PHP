1c35beb1b2222b0dfe39101b3c5db43324bec588
Oden <100082162+Oden909@users.noreply.github.com>
2025-02-27 18:43:52 +0300

    added lines on captcha

--- captcha.php
+++ captcha.php
@@ -11,7 +11,12 @@ $image_path = __DIR__ . '/img/noise.jpg';
 $image = imagecreatefromjpeg($image_path);
 
 $font_color = imagecolorallocate($image, 100, 100, 100);
-$font = 'C:/Windows/Fonts/BERNHC.TTF';
+$font = 'C:/Windows/Fonts/arial.ttf';
+$line_color = imagecolorallocate($image, 100, 100, 100);
+
+for ($i = 0; $i < 10; $i++) {
+    imageline($image, rand(0, imagesx($image)), rand(0, imagesy($image)), rand(0, imagesx($image)), rand(0, imagesy($image)), $line_color);
+}
 
 for ($i = 0; $i < strlen($captcha_text); $i++) {
     imagettftext(
